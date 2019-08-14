a3a6826dffecb94a606aa80ae5afd9b2c164d074
Michael Mulich <michael.mulich@gmail.com>
2019-08-14 15:29:45 -0700

    Unmake the bridge as a concourse resource
    This removes the bits that make the bridge code a concourse resource.
    It turns out that we need to be able to pass in the ident-hash value
    to the task, which is something that can't be done during a 'get step'
    but it can be done as part of a 'task step'.

--- bridge/README.md
+++ bridge/README.md
@@ -6,64 +6,21 @@ This application extracts content from the database to the filesystem. Deployed 
 
 See the Dockerfile for custom installation. <;0)
 
+Otherwise use `make build-image` to build the image.
+
+See `make help` for additional commands.
+
 ## Usage
 
-### Within the compose environment
-
-Usage within the context of `../docker-compose.yml`:
+### As an App
 
 ```
-docker-compose up -d
-cat << EOF > get-stdin.json
-{"source": {
-    "db": "postgresql://rhaptos@cnx-db/repository"
-},
- "params": {
-     "ident_hash": "4b5aaf32-1de1-419b-bdc0-4e0a7f6daf0f@27"
- }
-}
-EOF
-# This essentially is what the Concourse `get` task calls
-cat get-stdin.json | docker-compose exec -T bridge-resource /bin/bash -c "mkdir -p /tmp/output && /opt/resource/out /tmp/output && ls -lah /tmp/output"
+bridge --help
 ```
 
-### Within Concourse
+### As a Concourse task
 
-See the example in `../concourse/show-extracted-content.yml`.
-
-
-## Concourse Interface
-
-This project acts as a Concourse Resource for extracting content from the CNX Database.
-
-## Source Configuration
-
-* `db`: The database connection URL (e.g. `postgresql://rhaptos@cnx-db/repository`)
-
-## Behavior
-
-### `check`: Produce timestamps satisfying the interval.
-
-A no-op by returning a timestamp.
-
-### `in`: Report the given time.
-
-Extracts the content specified by the given `ident_hash`. The contents of `index.cnxml`, `index.cnxml.html` and any resource files are extracted to `$1`.
-
-The files in `$1` are identified by their SHA1 hash. These files also have an accompanying metadata file named `<SHA1>.metadata.json`.
-
-#### Parameters
-
-* `ident_hash`: The ident-hash of a specific piece of content (page or book) (e.g. `4b5aaf32-1de1-419b-bdc0-4e0a7f6daf0f@27`)
-
-### `out`: Produce the current time.
-
-Not implemented
-
-#### Parameters
-
-*None.*
-
+Execute any of the tasks in `./tasks` directory, which are also mounted within the container at `/tasks`.
 
 ## License
 
--- bridge/src/bridge/script.py
+++ bridge/src/bridge/script.py
@@ -1,12 +1,10 @@
 import json
-import sys
 from pathlib import Path
-from datetime import datetime
 
+import click
 import psycopg2
 
-from ..concourse import concourse_method
-from ..utils import get_resources, get_content, ContentNotFoundError
+from .utils import get_resources, get_content, ContentNotFoundError
 
 
 def gen_resource_filepath(dest_dir, sha1):
@@ -17,11 +15,14 @@ def gen_resource_metadata_filepath(dest_dir, sha1):
     return dest_dir / f'{sha1}.metadata.json'
 
 
-@concourse_method(required_source=('db',), required_params=('ident_hash',))
-def main(input_, environ, args):
-    dest_dir = Path(args[0])
-    db_url = input_['source']['db']
-    ident_hash = input_['params']['ident_hash']
+@click.command()
+@click.option('-o', '--output-dir', default='.',
+              type=click.Path(exists=True, file_okay=False, writable=False))
+@click.argument('ident_hash')
+@click.argument('db_url', envvar='DB_URL')
+def extract(output_dir, ident_hash, db_url):
+    """Extracts the requested content by ``ident_hash`` to the filesystem"""
+    dest_dir = Path(output_dir).resolve()
 
     with psycopg2.connect(db_url) as db_conn:
         with db_conn.cursor() as cursor:
@@ -46,19 +47,18 @@ def main(input_, environ, args):
                     json.dump(metadata, f)
 
             # Store the module metadata in a json file
-            try:
-                content = get_content(cursor, ident_hash)
-            except ContentNotFoundError as e:
-                sys.stderr.write(str(e) + '\n')
-                sys.exit(1)
+            content = get_content(cursor, ident_hash)
             with (dest_dir / 'archive-response.raw.json').open('w') as f:
                 f.write(json.dumps(content, sort_keys=True, indent=2))
 
-    version = {'timestamp': str(datetime.now().timestamp())}
-    output = {
-        'version': version,
-        'metadata': [
-            {'name': 'ident_hash', 'value': ident_hash},
-        ]
-    }
-    return output
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(extract)
+    
+
+if __name__ == '__main__':
+    main()
